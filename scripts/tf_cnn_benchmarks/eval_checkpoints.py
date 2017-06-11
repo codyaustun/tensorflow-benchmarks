72c2040498ef79cb0ff16d674a1dd07deadda2dc
Deepak Narayanan <deepakn94@gmail.com>
2017-06-11 23:35:41 +0000

    Small bug fixes to eval_checkpoints.py script

--- scripts/tf_cnn_benchmarks/eval_checkpoints.py
+++ scripts/tf_cnn_benchmarks/eval_checkpoints.py
@@ -5,16 +5,16 @@ import time
 
 def main(input_path, command):
   cnt = 0
-  with open(os.path.join(output_path, "README.md"), 'r') as f:
+  with open(os.path.join(input_path, "README.md"), 'r') as f:
     output = f.read()
-    time_interval = int(output.split(" = ")[1].rstrip(" secs"))
+    time_interval = int(output.split(" = ")[1].strip().rstrip(" secs"))
   accuracies = []
   while True:
     ckpt_path = ("%5d" % cnt).replace(' ', '0')
-    full_output_path = os.path.join(output_path, ckpt_path)
-    if not os.path.exists(full_output_path):
+    full_ckpt_path = os.path.join(input_path, ckpt_path)
+    if not os.path.exists(full_ckpt_path):
       break
-    full_command = command + " --train_dir=%s" % full_output_path
+    full_command = command + " --train_dir=%s 2>/dev/null" % full_ckpt_path
     output = subprocess.check_output(full_command, shell=True)
     for line in output.split('\n'):
       if "Precision" in line and "recall" in line:
@@ -23,8 +23,10 @@ def main(input_path, command):
         recall_at_5 = float(tokens[9])
         accuracies.append([(cnt + 1) * time_interval, precision_at_1, recall_at_5])
     cnt += 1
+
+  print "Time (in secs)\tTop 1 accuracy\tTop 5 accuracy"
   for accuracy in accuracies:
-    print "\t".join(accuracy)
+    print "\t".join([str(accuracy_token) for accuracy_token in accuracy])
 
 
 if __name__ == '__main__':
