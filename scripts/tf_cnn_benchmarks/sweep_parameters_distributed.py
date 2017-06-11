782db9f923fd47c52861e2275deac5d3365ccf0b
Deepak Narayanan <deepakn94@gmail.com>
2017-06-11 22:35:24 +0000

    sweep_parameters_distributed.py now working

--- scripts/tf_cnn_benchmarks/sweep_parameters_distributed.py
+++ scripts/tf_cnn_benchmarks/sweep_parameters_distributed.py
@@ -1,24 +1,24 @@
 import argparse
 import subprocess
-import threading
+import multiprocessing
 
-lock = threading.Lock()
+lock = multiprocessing.Lock()
 
 def spawn_process(command, server, docker_image, filename, return_output):
   # TODO: Don't start a new docker container here; have a setup phase where docker containers
   # are started; then have a teardown phase where docker containers are killed.
-  docker_cmd = """docker run -v /mnt:/mnt --net=host %s /bin/bash -c 'cd ~/tensorflow_benchmarks/scripts/tf_cnn_benchmarks; %s 2>/dev/null'""" % (docker_image, command)
-  ssh_cmd = "ssh -n %s -o StrictHostKeyChecking=no \"%s\"" % (server, docker_cmd))
+  docker_cmd = """nvidia-docker run -v /mnt:/mnt --net=host %s /bin/bash -c 'cd ~/tensorflow_benchmarks/scripts/tf_cnn_benchmarks; %s 2>/dev/null'""" % (docker_image, command)
+  ssh_cmd = "ssh -n %s -o StrictHostKeyChecking=no \"%s\"" % (server, docker_cmd)
   if return_output:
     output = subprocess.check_output(ssh_cmd, shell=True)
     with lock:
-        with open(filename, 'w') as f:
+        with open(filename, 'a') as f:
             f.write(output + "\n")
   else:
-    subprocess.call(ssh_cmd, shell=True)
+    subprocess.check_output(ssh_cmd, shell=True)
 
-def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes, output_folder,
-         docker_image):
+def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes,
+         output_folder, docker_image):
   subprocess.call("rm -rf %s; mkdir -p %s" % (output_folder, output_folder),
                   shell=True)
   throughputs = dict()
@@ -27,6 +27,7 @@ def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes, ou
     subprocess.call("mkdir -p %s/batch_size=%d" % (output_folder, batch_size),
                     shell=True)
     for num_nodes in all_num_nodes:
+      # Log command to output file.
       filename = "%s/batch_size=%d/gpus=%d.out" % (output_folder, batch_size, num_nodes)
       with open(filename, 'w') as f:
         command_to_execute = command
@@ -35,37 +36,59 @@ def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes, ou
         command_to_execute = command + " --worker_hosts=%s --ps_hosts=%s --num_gpus=%d --batch_size=%d" % (
           worker_hosts, ps_hosts, num_gpus_per_node, batch_size)
         worker_commands = ["%s --job_name=worker --task_index=%d" % (command_to_execute, i) for i in xrange(num_nodes)]
-        ps_commands = ["CUDA_VISIBLE_DEVICES='' %s -- job_name=ps --task_index=%d" % (
+        ps_commands = ["CUDA_VISIBLE_DEVICES='' %s --job_name=ps --task_index=%d" % (
           command_to_execute, i) for i in xrange(num_nodes)]
-        f.write("%s\n\n" % ("\n".join(worker_commands))
+        f.write("%s\n\n" % ("\n".join(worker_commands)))
+
+      # Start PS threads.
+      ps_threads = []
+      for i in xrange(num_nodes):
+        ps_thread = multiprocessing.Process(
+          target=spawn_process,
+          args=(ps_commands[i], servers[i], docker_image, filename, False))
+        ps_threads.append(ps_thread)
+        ps_thread.start()
+
+      # Start worker threads.
+      worker_threads = []
+      for i in xrange(num_nodes):
+        worker_thread = multiprocessing.Process(
+          target=spawn_process,
+          args=(worker_commands[i], servers[i], docker_image, filename, True))
+        worker_threads.append(worker_thread)
+        worker_thread.start()
 
       for i in xrange(num_nodes):
-        helper_thread = threading.Thread(
-          spawn_process, ps_commands[i], servers[i], docker_image, filename, False)
-        helper_thread.start()
-
-      helper_threads = []
-      for i in xrange(num_nodes):
-        helper_thread = threading.Thread(
-          spawn_process, worker_commands[i], servers[i], docker_image, filename, True)
-        helper_threads.append(helper_thread)
-        helper_thread.start()
+        worker_threads[i].join()
 
       for i in xrange(num_nodes):
-        helper_threads[i].join()
+        ps_threads[i].terminate()
+
+      # Kill the running docker container with the parameter server instance.
+      for i in xrange(num_nodes):
+        cmd = "nvidia-docker kill $(nvidia-docker ps -q)"
+        subprocess.check_output("ssh -n %s -o StrictHostKeyChecking=no '%s'" % (servers[i],
+                                                                                cmd),
+                                shell=True)
 
       with open(filename, 'r') as f:
         lines = f.read().split('\n')
         for line in lines:
           if "total images/sec:" in line:
             throughput = float(line.split(": ")[1].strip())
-            throughputs[(batch_size, num_gpus)] = throughput
+            if (batch_size, num_nodes) in throughputs:
+              if throughput > throughputs[(batch_size, num_nodes)]:
+                throughputs[(batch_size, num_nodes)] = throughput
+            else:
+              throughputs[(batch_size, num_nodes)] = throughput
 
-  print "\t".join([""] + ["# GPUs = %d" % num_gpus for num_gpus in all_num_gpus])
+  # Log summary to output.
+  print "\t".join([""] + ["# GPUs = %d" % num_nodes * num_gpus_per_node
+                          for num_nodes in all_num_nodes])
   for batch_size in all_batch_sizes:
     values = ["Batch size = %d" % batch_size]
-    for num_gpus in all_num_gpus:
-      values.append(str(throughputs[(batch_size, num_gpus)]))
+    for num_nodes in all_num_nodes:
+      values.append(str(throughputs[(batch_size, num_nodes)]))
     print "\t".join(values)
 
 
@@ -92,4 +115,5 @@ if __name__ == '__main__':
   opt_dict = vars(cmdline_args)
 
   main(opt_dict["command"], opt_dict["servers"], opt_dict["num_gpus_per_node"],
-       opt_dict["num_nodes"], opt_dict["batch_size"], opt_dict["output_folder"])
+       opt_dict["num_nodes"], opt_dict["batch_size"], opt_dict["output_folder"],
+       opt_dict["docker_image"])
