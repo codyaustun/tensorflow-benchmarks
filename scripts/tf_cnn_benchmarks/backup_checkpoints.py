60c53a559710f2646533b9ba907eaf53172fe804
Deepak Narayanan <deepakn94@gmail.com>
2017-06-11 22:55:38 +0000

    Log time interval in output directory in backup_checkpoints.py

--- scripts/tf_cnn_benchmarks/backup_checkpoints.py
+++ scripts/tf_cnn_benchmarks/backup_checkpoints.py
@@ -5,7 +5,9 @@ import time
 
 def main(input_path, output_path, time_interval):
   cnt = 0
-  subprocess.call("mkdir -p %s" % output_path, shell=True)
+  subprocess.call("rm -rf %s; mkdir -p %s" % (output_path, output_path), shell=True)
+  subprocess.call("echo \"Time interval = %d secs\" > %s" % (time_interval,
+                  os.path.join(output_path, "README.md")))
   while True:
     time.sleep(time_interval)
     ckpt_path = ("%5d" % cnt).replace(' ', '0')
