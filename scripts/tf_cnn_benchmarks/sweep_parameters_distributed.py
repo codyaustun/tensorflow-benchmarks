3f345a0303ea3399b9abccf4faccb5e1e5c6892b
Deepak Narayanan <deepakn94@gmail.com>
2017-06-12 03:56:40 +0000

    Call backup_checkpoints.py from sweep_parameters_distributed.py

--- scripts/tf_cnn_benchmarks/sweep_parameters_distributed.py
+++ scripts/tf_cnn_benchmarks/sweep_parameters_distributed.py
@@ -18,7 +18,7 @@ def spawn_process(command, server, docker_image, filename, return_output):
     subprocess.check_output(ssh_cmd, shell=True)
 
 def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes,
-         output_folder, docker_image):
+         output_folder, docker_image, input_path, output_path, time_interval):
   subprocess.call("rm -rf %s; mkdir -p %s" % (output_folder, output_folder),
                   shell=True)
   throughputs = dict()
@@ -31,6 +31,8 @@ def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes,
       filename = "%s/batch_size=%d/gpus=%d.out" % (output_folder, batch_size, num_nodes)
       with open(filename, 'w') as f:
         command_to_execute = command
+        if input_path is not None:
+          command_to_execute += " --train_dir=%s" % input_path
         worker_hosts = ",".join(["%s:50000" % servers[i] for i in xrange(num_nodes)])
         ps_hosts = ",".join(["%s:50001" % servers[i] for i in xrange(num_nodes)])
         command_to_execute = command + " --worker_hosts=%s --ps_hosts=%s --num_gpus=%d --batch_size=%d" % (
@@ -58,11 +60,23 @@ def main(command, servers, num_gpus_per_node, all_num_nodes, all_batch_sizes,
         worker_threads.append(worker_thread)
         worker_thread.start()
 
+      checkpoint_thread = None
+      if input_path is not None and output_path is not None:
+        checkpoint_command = "python backup_checkpoints.py -i %s -o %s -t %d" % (
+          input_path, output_path, time_interval)
+        checkpoint_thread = multiprocessing.Process(
+          target=spawn_process,
+          args=(checkpoint_command, servers[0], docker_image, filename, False))
+        checkpoint_thread.start()
+
       for i in xrange(num_nodes):
         worker_threads[i].join()
 
       for i in xrange(num_nodes):
         ps_threads[i].terminate()
+
+      if checkpoint_thread is not None:
+        checkpoint_thread.terminate()
 
       # Kill the running docker container with the parameter server instance.
       for i in xrange(num_nodes):
@@ -110,10 +124,17 @@ if __name__ == '__main__':
                       help="Output folder to dump logs")
   parser.add_argument('-d', "--docker_image", type=str, required=True,
                       help="Name of docker image")
+  parser.add_argument('-i', "--input_path", type=str, default=None,
+                      help="Training directory where checkpoints are dumped")
+  parser.add_argument('-p', "--output_path", type=str, default=None,
+                      help="Path to dump checkpointed models at regular intervals of time")
+  parser.add_argument('-t', "--time_interval", type=int, default=900,
+                      help="Time interval (in seconds) between model checkpoint dumps")
 
   cmdline_args = parser.parse_args()
   opt_dict = vars(cmdline_args)
 
   main(opt_dict["command"], opt_dict["servers"], opt_dict["num_gpus_per_node"],
        opt_dict["num_nodes"], opt_dict["batch_size"], opt_dict["output_folder"],
-       opt_dict["docker_image"])
+       opt_dict["docker_image"], opt_dict["input_path"], opt_dict["output-path"],
+       opt_dict["time_interval"])
