5a39e68701833c88b065f223dc6368ace8a78f41
Deepak Narayanan <deepakn94@gmail.com>
2017-06-14 06:01:32 +0000

    Remove the need to pass in '--temp_file' parameter

--- scripts/tf_cnn_benchmarks/eval_checkpoints.py
+++ scripts/tf_cnn_benchmarks/eval_checkpoints.py
@@ -4,11 +4,13 @@ import subprocess
 import sys
 import time
 
-def main(input_path, temp_path, command):
+def main(input_path, command):
   cnt = 0
   with open(os.path.join(input_path, "README.md"), 'r') as f:
     output = f.read()
-    time_interval = int(output.split(" = ")[1].strip().rstrip(" secs"))
+    output_lines = output.split('\n')
+    time_interval = int(output_lines[0].split(" = ")[1].strip().rstrip(" secs"))
+    original_path = output_lines[1].split(" = ")[1].strip()
   print "Time (in secs)\tNumber of minibatches\tTop 1 accuracy\tTop 5 accuracy"
   while True:
     ckpt_path = ("%5d" % cnt).replace(' ', '0')
@@ -19,9 +21,9 @@ def main(input_path, temp_path, command):
       cnt += 1
       continue
     subprocess.check_output("mkdir -p %s; rm %s/*; cp %s/* %s" %
-                            (temp_path, temp_path, full_ckpt_path, temp_path),
+                            (original_path, original_path, full_ckpt_path, original_path),
                             shell=True)
-    full_command = command + " --train_dir=%s 2>/dev/null" % temp_path
+    full_command = command + " --train_dir=%s 2>/dev/null" % original_path
     output = subprocess.check_output(full_command, shell=True)
     for line in output.split('\n'):
       if "Precision" in line and "Recall" in line:
@@ -41,12 +43,10 @@ if __name__ == '__main__':
   )
   parser.add_argument('-i', "--input_path", type=str, required=True,
                       help="Path to dumped model checkpoints")
-  parser.add_argument('-t', "--temp_path", type=str, required=True,
-                      help="Path where model was initially dumped to")
   parser.add_argument('-c', "--command", type=str, required=True,
                       help="Command to evaluate each individual checkpoint")
 
   cmdline_args = parser.parse_args()
   opt_dict = vars(cmdline_args)
 
-  main(opt_dict["input_path"], opt_dict["temp_path"], opt_dict["command"])
+  main(opt_dict["input_path"], opt_dict["command"])
